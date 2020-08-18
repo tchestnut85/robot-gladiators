5829572e9c64a4b1a96059f5ac99a483f7b147bf
Tom Chestnut <tchestnut85@gmail.com>
2020-08-17 20:38:02 -0400

    added control flow to the function so a full round of fighting is in the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,7 @@ var playerAttack = 10;
 console.log(playerName, playerHealth, playerAttack);
 
 var enemyName = "Roborto";
-var enemyHealth = "50";
+var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
@@ -19,13 +19,30 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    platerHealth = playerHealth - enemyAttack;
+    playerHealth = playerHealth - enemyAttack;
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     )
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 fight ();
